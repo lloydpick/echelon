4733ae93ca70545033b7799d857988073143d998
Lloyd Pick <lloydpick@gmail.com>
2010-10-20 04:24:53 +0100

    added concept of a ride

---
+++ lib/echelon/ride.rb
@@ -0,0 +1,11 @@
+module Echelon
+  class Ride
+    attr_reader :name, :queue_time, :active
+
+    def initialize(*params)
+      params.each do |key|
+        key.each { |k, v| instance_variable_set("@#{k}", v) }
+      end
+    end
+  end
+end
