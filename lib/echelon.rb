3a1c10536786209f3dfafd5211d62c8d9b4d207a
Lloyd Pick <lloydpick@gmail.com>
2010-11-27 03:49:46 +0000

    bumped the versions

--- lib/echelon.rb
+++ lib/echelon.rb
@@ -6,5 +6,5 @@ require "#{File.dirname(__FILE__)}/echelon/thorpe_park.rb"
 require "#{File.dirname(__FILE__)}/echelon/disneyland_paris.rb"
 
 module Echelon
-  VERSION = '0.0.1'
+  VERSION = '0.0.2'
 end
