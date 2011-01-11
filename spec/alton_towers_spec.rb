e6f3fc41d7da1f54cc73df7447b63076713de4bf
Lloyd Pick <lloydpick@gmail.com>
2011-01-11 16:35:05 +0000

    alton towers spec

---
+++ spec/alton_towers_spec.rb
@@ -0,0 +1,34 @@
+require 'echelon/park'
+require 'echelon/parks/alton_towers'
+
+# Mosts of the test here are pretty generic, although you can test for specific
+# cases, such as the active states may differ between parks, and queue times may
+# not exceed a certain value etc etc.
+
+describe Echelon::AltonTowers do
+
+  before do
+    @park = Echelon::AltonTowers.new()
+  end
+
+  it "should inherit from Park" do
+    @park.should be_kind_of(Echelon::AltonTowers)
+  end
+
+  it "should have some rides" do
+    @park.ride_list.count.should satisfy { |v| v > 1 && v < 30 }
+  end
+
+  it "should return ride objects" do
+    @park.find_by_name("Rita").should be_kind_of(Echelon::Ride)
+    @park.find_by_id(3).should be_kind_of(Echelon::Ride)
+  end
+
+  it "should return ride object values correctly" do
+    stealth = @park.find_by_id(3)
+    stealth.name.should eql("Rita")
+    stealth.queue_time.should satisfy { |v| v >= 0 && v < 1000 }
+    stealth.active.should satisfy { |v| v == 0 || v == 1 }
+  end
+
+end
