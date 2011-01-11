2ca3ef076df5ed00823a8687dfd0e9bc963e8d48
Lloyd Pick <lloydpick@gmail.com>
2011-01-11 16:37:37 +0000

    alton towers park code (although remote data not currently in use/operational)

---
+++ lib/echelon/parks/alton_towers.rb
@@ -0,0 +1,40 @@
+require 'rubygems'
+require 'json'
+require 'net/http'
+
+module Echelon
+  class AltonTowers < Park
+
+    attr_reader :json_data
+
+    def ride_list
+      {
+        1 => 'Air',
+        2 => 'Enterprise',
+        3 => 'Rita'
+      }
+    end
+
+    def initialize
+      # fetch the json feed from the merlin site
+      url = "http://www.merlincms.com/2.php"
+      resp = Net::HTTP.get_response(URI.parse(url))
+      data = resp.body
+
+      # were only interested in the ride data, throw everything else away
+      json_data = JSON.parse(data)
+      @json_data = json_data["Rides"]
+    end
+
+    private
+
+    def create_ride_object(ref)
+      self.json_data.each do |ride|
+        if ride["ref"].to_i == ref
+          return Ride.new(:name => self.ride_list[ref], :queue_time => ride["queue"].to_i, :active => ride["active"].to_i)
+        end
+      end
+    end
+
+  end
+end
