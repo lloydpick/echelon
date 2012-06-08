1973560e2672d94ea9e0904b0a24357391e2764a
Lloyd Pick <lloydpick@gmail.com>
2012-06-08 23:54:34 +0100

    name the seaworld park files

--- lib/echelon.rb
+++ lib/echelon.rb
@@ -4,12 +4,14 @@ $:.unshift(File.dirname(__FILE__)) unless
 require "#{File.dirname(__FILE__)}/echelon/ride.rb"
 require "#{File.dirname(__FILE__)}/echelon/park.rb"
 
+# UK Parks
 require "#{File.dirname(__FILE__)}/echelon/parks/thorpe_park.rb"
-require "#{File.dirname(__FILE__)}/echelon/parks/disneyland_paris.rb"
-require "#{File.dirname(__FILE__)}/echelon/parks/seaworld_san_antonio.rb"
-require "#{File.dirname(__FILE__)}/echelon/parks/seaworld_san_diego.rb"
-require "#{File.dirname(__FILE__)}/echelon/parks/seaworld_orlando.rb"
 require "#{File.dirname(__FILE__)}/echelon/parks/alton_towers.rb"
+
+# Seaworld
+require "#{File.dirname(__FILE__)}/echelon/parks/seaworld/san_antonio.rb"
+require "#{File.dirname(__FILE__)}/echelon/parks/seaworld/san_diego.rb"
+require "#{File.dirname(__FILE__)}/echelon/parks/seaworld/orlando.rb"
 
 # Disney World Resort
 require "#{File.dirname(__FILE__)}/echelon/parks/disney_world/magic_kingdom.rb"
@@ -17,9 +19,12 @@ require "#{File.dirname(__FILE__)}/echelon/parks/disney_world/epcot.rb"
 require "#{File.dirname(__FILE__)}/echelon/parks/disney_world/hollywood_studios.rb"
 require "#{File.dirname(__FILE__)}/echelon/parks/disney_world/animal_kingdom.rb"
 
-# Disneyland Resort
+# Disneyland Resort California
 require "#{File.dirname(__FILE__)}/echelon/parks/disneyland/disneyland.rb"
 require "#{File.dirname(__FILE__)}/echelon/parks/disneyland/california_adventure.rb"
+
+# Disneyland Resort Paris
+require "#{File.dirname(__FILE__)}/echelon/parks/disneyland_paris.rb"
 
 module Echelon
 end
