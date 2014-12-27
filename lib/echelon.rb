003910de2b0e65ccbda70d6c321bf3b8dd7ba964
Lloyd Pick <lloydpick@gmail.com>
2014-12-27 14:56:20 +0000

    remove commented code

--- lib/echelon.rb
+++ lib/echelon.rb
@@ -15,10 +15,6 @@ require "#{File.dirname(__FILE__)}/echelon/parks/seaworld/orlando.rb"
 
 # Disney World Resort
 require "#{File.dirname(__FILE__)}/echelon/parks/disney_world.rb"
-# require "#{File.dirname(__FILE__)}/echelon/parks/disney_world/magic_kingdom.rb"
-# require "#{File.dirname(__FILE__)}/echelon/parks/disney_world/epcot.rb"
-# require "#{File.dirname(__FILE__)}/echelon/parks/disney_world/hollywood_studios.rb"
-# require "#{File.dirname(__FILE__)}/echelon/parks/disney_world/animal_kingdom.rb"
 
 # Disneyland Resort California
 require "#{File.dirname(__FILE__)}/echelon/parks/disneyland/disneyland.rb"
